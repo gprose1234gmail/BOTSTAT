e8d823d1a8c9bffaabf385a1fba1dd921d9cec6c
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-02-11 09:00:55 +0530

    Update botstatus_pratheek.py

--- botstatus_pratheek.py
+++ botstatus_pratheek.py
@@ -23,7 +23,7 @@ async def main_pratheek():
                 GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
                 CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
                 CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
-                xxx_pratheek = f"📊 𝙇𝙄𝙑𝙀 𝘽𝙊𝙏 𝙎𝙏𝘼𝙏𝙐𝙎\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
+                xxx_pratheek = f"𝙇𝙐𝙉𝘼 𝘽𝙊𝙏𝙎 𝙎𝙏𝘼𝙏𝙐𝙎 :\n\n**𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗟𝗨𝗡𝗔**: 𝙏𝙝𝙞𝙨 𝙞𝙨 𝙡𝙞𝙫𝙚 𝙨𝙩𝙖𝙩𝙪𝙨 𝙤𝙛 𝙖𝙡𝙡 𝙇𝙪𝙣𝙖 𝘽𝙤𝙩𝙨. 𝙏𝙝𝙞𝙨 𝙈𝙚𝙨𝙨𝙖𝙜𝙚 𝙠𝙚𝙚𝙥𝙨 𝙤𝙣 𝙪𝙥𝙙𝙖𝙩𝙞𝙣𝙜 𝙞𝙣 𝙚𝙫𝙚𝙧𝙮 45 𝙢𝙞𝙣𝙨 𝙬𝙞𝙩𝙝 𝙡𝙞𝙫𝙚 𝙨𝙩𝙖𝙩𝙪𝙨 𝙤𝙛 𝙖𝙡𝙡 𝙗𝙤𝙩𝙨."
                 for bot in BOT_LIST:
                     try:
                         yyy_pratheek = await app.send_message(bot, "/start")
@@ -33,21 +33,21 @@ async def main_pratheek():
                         for ccc in zzz_pratheek:
                             bbb = ccc.message_id
                         if aaa == bbb:
-                            xxx_pratheek += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: down ❌"
+                            xxx_pratheek += f"\n\n💕 **𝗟𝗨𝗡𝗔**: @{bot}\n💟 **𝗟𝗜𝗩𝗘**: I M AT REST  💤💤"
                             for bot_admin_id in BOT_ADMIN_IDS:
                                 try:
-                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep!! @{bot} is down** ❌")
+                                    await app.send_message(int(bot_admin_id), f"💟 **SORRY GUYS SOME ISSUE !! @{bot} is AT SLEEP** 💤💤")
                                 except Exception:
                                     pass
                             await app.read_history(bot)
                         else:
-                            xxx_pratheek += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: alive ✅"
+                            xxx_pratheek += f"\n\n💕 **𝗟𝗨𝗡𝗔**: @{bot}\n💟 **𝗟𝗜𝗩𝗘**: alive 🔥"
                             await app.read_history(bot)
                     except FloodWait as e:
                         await asyncio.sleep(e.x)            
                 time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
                 last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45min</i> - Made By [Dᴇᴠᴏʟᴏᴘᴇʀ🥀](https://t.me/alpha_romeo_06) 💙"
+                xxx_pratheek += f"\n\n✔️ 𝗟𝗮𝘀𝘁 𝗰𝗵𝗲𝗰𝗸𝗲𝗱 𝗼𝗻: {last_update} ({TIME_ZONE})\n\n<i>💘 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 𝗲𝘃𝗲𝗿𝘆 𝟰𝟱𝗺𝗶𝗻 </i> - Made By||[Dᴇᴠᴏʟᴏᴘᴇʀ🥀](https://t.me/alpha_romeo_06 ) 💙||/n/n #TRYLUNA"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_pratheek)
                 print(f"Last checked on: {last_update}")                
                 await asyncio.sleep(2700)
